058ce354fd06bf93b371ba80cfece9f3c85c6da5
nathan-stamand <nathansaint810@gmail.com>
2020-06-30 16:33:19 -0500

    added #index, #create, #show, and private #user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: UserSerializer.new(users)
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: UserSerializer.new(user)
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: UserSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_digest)
+  end
 end
