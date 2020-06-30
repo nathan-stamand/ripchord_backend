d8daa237f2c8a73bb193f8b3f1969d22e81b001c
nathan-stamand <nathansaint810@gmail.com>
2020-06-30 13:26:26 -0500

    added namespacing for :api and :v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :songs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do
+      resources :users, :songs
+    end
+  end
 end
