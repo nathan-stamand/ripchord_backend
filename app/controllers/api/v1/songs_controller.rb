8c7c057d519373559f7bf24387a1615808400e32
nathan-stamand <nathansaint810@gmail.com>
2020-06-30 13:26:01 -0500

    added namespacing for clarity and moved controllers into app/controllers/api/v1

---
+++ app/controllers/api/v1/songs_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: SongSerializer.new(songs)
+  end
+
+  def create
+    song = Song.create(song_params)
+    render json: SongSerializer.new(song)
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: SongSerializer.new(song)
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.update(song_params)
+    render json: SongSerializer.new(song)
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.destroy
+    render json: {songId: song.id}
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :chords, :user_id)
+  end
+end
