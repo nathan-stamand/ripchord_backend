8b411ffa0cb3423780f7c842556ed87b569309e7
nathan-stamand <nathansaint810@gmail.com>
2020-07-01 14:27:50 -0500

    restructured routes and added login/logout for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
-      resources :users, :songs
+      get "/users", to: "users#index"
+      get "/login", to: "users#login"
+      post "/login", to: "users#login"
+      post "/logout", to: "users#logout"
+      post "/users", to: "users#create"
+      post "/users/:id", to: "users#show"
+      resources :songs
     end
   end
 end
